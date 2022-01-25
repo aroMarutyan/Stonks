7a5355a10aed380a969026809549143112fe0f16
Deathrovv <aro.maru7ian@gmail.com>
2022-01-25 15:26:24 +0100

    final commit

--- antiCramerBot/functions/index.js
+++ antiCramerBot/functions/index.js
@@ -1,30 +1,99 @@
-// import { OPEN_AI_KEY, OPEN_AI_ORG } from "../apiKeys";
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//// SDK Config ////
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  organization: functions.config().openai.id,
-  apiKey: functions.config().openai.key,
+  organization: functions.config().openai.id, // Need to deploy function for this to work it seems. Can't deploy without chaning firebase payment plan
+  apiKey: functions.config().openai.key, // Need to deploy function for this to work it seems. Can't deploy without chaning firebase payment plan
 });
-
 const openai = new OpenAIApi(configuration);
 
-exports.helloJim = functions.https.onRequest((req, res) => {
-  const gptCompletion = openai.createCompletion("text-davinci-001", {
-    prompt: "Jim Cramer recommends selling the following stock tickers:",
-    temperature: 0.7,
-    max_tokens: 32,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  res.send(gptCompletion.data);
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+const alpaca = new Alpaca({
+  keyId: functions.config().alpaca.id, // Need to deploy function for this to work it seems. Can't deploy without chaning firebase payment plan
+  secretKey: functions.config().alpaca.key, // Need to deploy function for this to work it seems. Can't deploy without chaning firebase payment plan
+  // paper: true,
 });
+
+//// PUPPETEER Scrape Data from Twitter for better AI context ////
+
+const puppeteer = require("puppeteer");
+
+async function scrape() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://twitter.com/jimcramer", {
+    waitUntil: "networkidle2",
+  });
+
+  await page.waitForTimeout(3000);
+
+  // await page.screenshot({ path: 'example.png' });
+
+  const tweets = await page.evaluate(async () => {
+    return document.body.innerText;
+  });
+
+  await browser.close();
+
+  return tweets;
+}
+
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+  // test logic here
+
+  response.send("test");
+});
+
+exports.getRichQuick = functions
+  .runWith({ memory: "4GB" })
+  .pubsub.schedule("0 10 * * 1-5")
+  .timeZone("America/New_York")
+  .onRun(async (ctx) => {
+    console.log("This will run M-F at 10:00 AM Eastern!");
+
+    const tweets = await scrape();
+
+    const gptCompletion = await openai.createCompletion("text-davinci-001", {
+      prompt: `${tweets} Jim Cramer recommends selling the following stock tickers: `,
+      temperature: 0.7,
+      max_tokens: 32,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const stocksToBuy = gptCompletion.data.choices[0].text.match(/\b[A-Z]+\b/g);
+    console.log(`Thanks for the tips Jim! ${stocksToBuy}`);
+
+    if (!stocksToBuy) {
+      console.log("sitting this one out");
+      return null;
+    }
+
+    //// ALPACA Make Trades ////
+
+    // close all positions
+    const cancel = await alpaca.cancelAllOrders();
+    const liquidate = await alpaca.closeAllPositions();
+
+    // get account
+    const account = await alpaca.getAccount();
+    console.log(`dry powder: ${account.buying_power}`);
+
+    // place order
+    const order = await alpaca.createOrder({
+      symbol: stocksToBuy[0],
+      // qty: 1,
+      notional: account.buying_power * 0.9, // will buy fractional shares
+      side: "buy",
+      type: "market",
+      time_in_force: "day",
+    });
+
+    console.log(`look mom i bought stonks: ${order.id}`);
+
+    return null;
+  });
